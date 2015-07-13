38b07a5ca8422769223ca86d6c1ac32590b044aa
asteph <alissa.stephens1@gmail.com>
2015-07-13 16:42:44 -0500

    allows user to designate range on which the random number will be generated upon running the file, or it will prompt them if they are missing. Not very elegant solution since the functions require these values to be passed to them

--- index.php
+++ index.php
@@ -4,31 +4,40 @@
 
 //generates random number and displays range for guess
 //calls guess() with randomNumber passed to it
-function playGame(){
-	$randomNumber = mt_rand(1, 100);
+if ($argc == 1){
+	fwrite(STDOUT, "Enter min value: \n");
+	$min = (int)fgets(STDIN);
+	fwrite(STDOUT, "Enter max value: \n");
+	$max = (int)fgets(STDIN);
+}else{
+	$min = (int)$argv[1];
+	$max = (int)$argv[2];	
+}
+function playGame($min, $max){
+	$randomNumber = mt_rand($min, $max);
 	fwrite(STDOUT, "Guess a number between 1 and 100.\n");
 	//keeps track of user guesses
 	$guessNumber = 0;
-	guess($randomNumber, $guessNumber);
+	guess($randomNumber, $guessNumber, $min, $max);
 }
 //asks for guess and compares to the random number that was 
 //passed to it and then calls response with
 //the parameters of the random number and the user number
-function guess($randomNumber, $guessNumber){
+function guess($randomNumber, $guessNumber, $min, $max){
 	fwrite(STDOUT, 'Guess? ');
 	$userNumber = fgets(STDIN);
 	$guessNumber++;
-	response($randomNumber, $userNumber, $guessNumber);
+	response($min, $max, $randomNumber, $userNumber, $guessNumber);
 }
 //compares guess to number and responds with higher or lower
 //if guess is correct, asks if user wants to play again
-function response($randomNumber, $userNumber, $guessNumber){
+function response($min, $max, $randomNumber, $userNumber, $guessNumber){
 		if($randomNumber < $userNumber){
 			fwrite(STDOUT, "LOWER\n");
-			guess($randomNumber, $guessNumber);
+			guess($randomNumber, $guessNumber, $min, $max);
 		}else if($randomNumber > $userNumber){
 			fwrite(STDOUT, "HIGHER\n");
-			guess($randomNumber, $guessNumber);
+			guess($randomNumber, $guessNumber, $min, $max);
 		}
 		else{
 			fwrite(STDOUT, "GOOD GUESS!\n");
@@ -39,10 +48,10 @@ function response($randomNumber, $userNumber, $guessNumber){
 			$playAgain = trim(fgets(STDIN));
 			if($playAgain == 'y'){
 				//reset the game
-				playGame();
+				playGame($min, $max);
 			}
 		}
 	}
 //run game
-playGame();
+playGame($min, $max);
 ?>
